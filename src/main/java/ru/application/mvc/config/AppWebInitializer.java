b5cd9f024355fd65e54ed3971698a4d5c48cce05
AntonyKon <kononenko150@gmail.com>
2021-07-12 17:44:47 +0300

    add configure classes

---
+++ src/main/java/ru/application/mvc/config/AppWebInitializer.java
@@ -0,0 +1,38 @@
+package ru.application.mvc.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class AppWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+
+
+        // Add some filters in configuration
+        servletContext.addFilter("hiddenMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
+        servletContext.addFilter("UTFFilter",
+                new CharacterEncodingFilter("UTF-8", true)).addMappingForUrlPatterns(null,
+                true, "/*");
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
