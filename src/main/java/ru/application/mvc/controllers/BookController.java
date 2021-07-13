f927f6d61cde503335fb235233649b023e263100
AntonyKon <kononenko150@gmail.com>
2021-07-13 23:07:52 +0300

    add some controllers

---
+++ src/main/java/ru/application/mvc/controllers/BookController.java
@@ -0,0 +1,10 @@
+package ru.application.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/books")
+@Controller
+public class BookController {
+}
