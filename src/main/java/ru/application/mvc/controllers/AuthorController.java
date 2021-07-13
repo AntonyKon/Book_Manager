53c057b04f50676b6a9cec197322d581a6adb248
AntonyKon <kononenko150@gmail.com>
2021-07-14 00:01:15 +0300

    create controllers for authors and genres table

---
+++ src/main/java/ru/application/mvc/controllers/AuthorController.java
@@ -0,0 +1,10 @@
+package ru.application.mvc.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/authors")
+public class AuthorController {
+}
