7167846322b0458ab6a1fee2290c8afdabc14a56
AntonyKon <kononenko150@gmail.com>
2021-07-14 21:40:53 +0300

    updated GenreController.java

--- src/main/java/ru/application/mvc/controllers/GenreController.java
+++ src/main/java/ru/application/mvc/controllers/GenreController.java
@@ -1,9 +1,23 @@
 package ru.application.mvc.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.application.mvc.dao.GenreDao;
 
 @Controller
 @RequestMapping("/genres")
 public class GenreController {
+
+    @Autowired
+    private GenreDao genreDao;
+
+    @GetMapping()
+    public String showGenres(Model model) {
+        model.addAttribute("genres", genreDao.findAll());
+
+        return "genres/index";
+    }
 }
