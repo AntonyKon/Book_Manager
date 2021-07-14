08de2160edbb092070692200333af3dbb6538dc0
AntonyKon <kononenko150@gmail.com>
2021-07-14 21:56:06 +0300

    add bootstrap

--- src/main/java/ru/application/mvc/controllers/GenreController.java
+++ src/main/java/ru/application/mvc/controllers/GenreController.java
@@ -4,20 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.application.mvc.dao.GenreDao;
+import ru.application.mvc.models.Genre;
+import ru.application.mvc.services.ManagementService;
 
 @Controller
 @RequestMapping("/genres")
 public class GenreController {
 
     @Autowired
-    private GenreDao genreDao;
+    private ManagementService service;
 
     @GetMapping()
     public String showGenres(Model model) {
-        model.addAttribute("genres", genreDao.findAll());
+        model.addAttribute("genres", service.findAllGenres());
 
         return "genres/index";
+    }
+
+    @GetMapping("/new")
+    public String addGenre(@ModelAttribute("genre") Genre genre) {
+        return "genres/new";
     }
 }
