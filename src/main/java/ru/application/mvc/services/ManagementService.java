f6262b96771abb5aa0603354bb7d92f28288ff5e
AntonyKon <kononenko150@gmail.com>
2021-08-28 18:00:05 +0300

    started to filling service for my application

--- src/main/java/ru/application/mvc/services/ManagementService.java
+++ src/main/java/ru/application/mvc/services/ManagementService.java
@@ -1,6 +1,8 @@
 package ru.application.mvc.services;
 
 
+import org.springframework.web.multipart.MultipartFile;
+import ru.application.mvc.models.Author;
 import ru.application.mvc.models.Book;
 import ru.application.mvc.models.Genre;
 
@@ -10,16 +12,23 @@ public interface ManagementService {
 
     Book findBookById(int id);
     List<Book> findAllBooks();
-    Book findBookByName(String name);
-    void saveBook(Book book);
+    List<Book> findBooksByName(String name);
+    void saveBook(Book book, MultipartFile file);
     void updateBook(Book book);
     void deleteBook(Book book);
 
     Genre findGenreById(int id);
     List<Genre> findAllGenres();
-    Genre findGenreByName(String name);
+    List<Genre> findGenresByName(String name);
     void saveGenre(Genre genre);
     void updateGenre(Genre genre);
     void deleteGenre(Genre genre);
+
+    abstract Author findAuthorById(int id);
+    abstract List<Author> findAuthorsByName(String name);
+    abstract List<Author> findAllAuthors();
+    abstract void saveAuthor(Author author);
+    abstract void updateAuthor(Author author);
+    abstract void deleteAuthor(Author author);
 
 }
--- src/main/java/ru/application/mvc/services/ManagementServiceImpl.java
+++ src/main/java/ru/application/mvc/services/ManagementServiceImpl.java
@@ -2,25 +2,35 @@ package ru.application.mvc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.application.mvc.dao.BookDao;
-import ru.application.mvc.dao.GenreDao;
+import org.springframework.web.multipart.MultipartFile;
+import ru.application.mvc.dao.GenericDao;
+import ru.application.mvc.models.Author;
 import ru.application.mvc.models.Book;
 import ru.application.mvc.models.Genre;
 
+import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ManagementServiceImpl implements ManagementService{
 
     @Autowired
-    private BookDao bookDao;
+    private GenericDao<Book> bookDao;
     @Autowired
-    private GenreDao genreDao;
+    private GenericDao<Genre> genreDao;
+    @Autowired
+    private GenericDao<Author> authorDao;
 
+    @Resource(mappedName = "fileStorage/basePath")
+    private String path;
 
     @Override
     public Book findBookById(int id) {
-        return null;
+
+        return bookDao.findById(id);
     }
 
     @Override
@@ -29,28 +39,29 @@ public class ManagementServiceImpl implements ManagementService{
     }
 
     @Override
-    public Book findBookByName(String name) {
-        return null;
+    public List<Book> findBooksByName(String name) {
+        return bookDao.findByName(name);
     }
 
     @Override
-    public void saveBook(Book book) {
-
+    public void saveBook(Book book, MultipartFile file) {
+        bookDao.save(book);
+        savePhotos(new MultipartFile[] {file});
     }
 
     @Override
     public void updateBook(Book book) {
-
+        bookDao.update(book);
     }
 
     @Override
     public void deleteBook(Book book) {
-
+        bookDao.delete(book);
     }
 
     @Override
     public Genre findGenreById(int id) {
-        return null;
+        return genreDao.findById(id);
     }
 
     @Override
@@ -59,8 +70,8 @@ public class ManagementServiceImpl implements ManagementService{
     }
 
     @Override
-    public Genre findGenreByName(String name) {
-        return null;
+    public List<Genre> findGenresByName(String name) {
+        return genreDao.findByName(name);
     }
 
     @Override
@@ -70,11 +81,50 @@ public class ManagementServiceImpl implements ManagementService{
 
     @Override
     public void updateGenre(Genre genre) {
-
+        genreDao.update(genre);
     }
 
     @Override
     public void deleteGenre(Genre genre) {
+        genreDao.delete(genre);
+    }
 
+    @Override
+    public Author findAuthorById(int id) {
+        return authorDao.findById(id);
+    }
+
+    @Override
+    public List<Author> findAuthorsByName(String name) {
+        return authorDao.findByName(name);
+    }
+
+    @Override
+    public List<Author> findAllAuthors() {
+        return authorDao.findAll();
+    }
+
+    @Override
+    public void saveAuthor(Author author) {
+    }
+
+    @Override
+    public void updateAuthor(Author author) {
+    }
+
+    @Override
+    public void deleteAuthor(Author author) {
+    }
+
+    public void savePhotos(MultipartFile[] files) {
+        for (MultipartFile file: files) {
+            File defaultFile = new File(path, Objects.requireNonNull(file.getOriginalFilename()));
+            try {
+                defaultFile.createNewFile();
+                file.transferTo(defaultFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
