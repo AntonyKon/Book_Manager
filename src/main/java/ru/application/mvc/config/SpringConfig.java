90f316f4ba126c31730d48fcf95e4e50c9d43d75
AntonyKon <kononenko150@gmail.com>
2021-08-28 17:57:04 +0300

    add new configuration in SpringConfig.java

--- src/main/java/ru/application/mvc/config/SpringConfig.java
+++ src/main/java/ru/application/mvc/config/SpringConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -52,6 +54,20 @@ public class SpringConfig implements WebMvcConfigurer {
 
         registry.viewResolver(resolver);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/images/");
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(25 * 8 * 1024 * 1024);
+        return multipartResolver;
+    }
+
+
 
 
 }
